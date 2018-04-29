6f450632b9eb58104580a7146f85514bf0462d6c
EthanGA <ethan.gossh@gmail.com>
2018-04-29 16:47:01 -0400

    first_transition
    reworked room1 and first transition

---
+++ Assets/tutorialGate.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tutorialGate : MonoBehaviour {
+
+	public bool playerEntered = false;
+
+	void OnTriggerEnter(Collider collider) {
+		
+		playerEntered = true;
+	}
+}
