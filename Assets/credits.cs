2bd910605faff577ae50949ff0b8c3989f349a8b
EthanGA <ethan.gossh@gmail.com>
2018-04-30 12:50:32 -0400

    Credits
    added credits screen

---
+++ Assets/credits.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class credits : MonoBehaviour {
+
+	public GameObject main;
+
+	void Update() {
+		if (Input.GetKeyDown(KeyCode.Escape)) {
+			main.SetActive(true);
+		}
+	}
+
+	public void ShowCredits() {
+		main.SetActive(false);
+	}
+}
