5d817e73d64e23a6428791f62aa03b9118cf89b7
EthanGA <ethan.gossh@gmail.com>
2018-05-02 10:10:58 -0400

    okay actually fixed it

---
+++ Assets/TutorialBlock.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialBlock : MonoBehaviour {
+	public Image pause;
+
+	void Update() {
+
+		if (pause.enabled == true) {
+			gameObject.SetActive(false);
+		} else {
+			gameObject.SetActive(true);
+		}
+	}
+}
