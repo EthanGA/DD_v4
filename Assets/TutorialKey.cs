e5fc488514480570ebbde38dd97b038b963b883f
EthanGA <ethan.gossh@gmail.com>
2018-05-03 15:17:20 -0400

    TUTORIAL DONE

---
+++ Assets/TutorialKey.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialKey : MonoBehaviour {
+
+	public GameObject player, torch1, torch2;
+	public bool tutorialComplete = false;
+	
+	void OnTriggerEnter(Collider collider) {
+		if (collider.gameObject == player) {
+			torch1.SetActive(true);
+			torch2.SetActive(true);
+			tutorialComplete = true;
+			gameObject.GetComponentInChildren<MeshRenderer>().enabled = false;
+		}
+	}
+}
