483e1da24bb6a164ed789cafabfba34966d42d96
EthanGA <ethan.gossh@gmail.com>
2018-05-03 10:07:52 -0400

    fixed pause menu bug

--- Assets/TutorialBlock.cs
+++ Assets/TutorialBlock.cs
@@ -5,14 +5,16 @@ using UnityEngine.UI;
 
 public class TutorialBlock : MonoBehaviour {
 	public Image pause;
-	public GameObject tutorial;
+	public GameObject tutorial1, tutorial2;
 
 	void Update() {
 
 		if (pause.isActiveAndEnabled == true) {
-			tutorial.SetActive(false);
+			tutorial1.SetActive(false);
+	//		tutorial2.SetActive(false);
 		} else {
-			tutorial.SetActive(true);
+			tutorial1.SetActive(true);
+//			tutorial2.SetActive(true);
 		}
 	}
 }
